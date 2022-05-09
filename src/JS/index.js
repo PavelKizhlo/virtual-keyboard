1ea52356488de7a61f34f6b26bccac1fe52674f2
Pavel Kizhlo <pavelkizh@gmail.com>
2022-05-10 00:31:21 +0300

    refactor: all listeners are moved to individual functions

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,6 +1,6 @@
 import { createPage } from './create-elements.js';
-import { pressKey } from './press.js';
-import { changeLayout, caseSwich, capsLockSwich } from './swich.js'
+import { changeLayout } from './swich.js';
+import { addWindowListeners, addKeyListeners } from './listeners.js';
 
 let lang = localStorage.getItem('lang');
 
@@ -14,88 +14,6 @@ if (lang === 'rus') {
 };
 
 
-const keyboard = document.querySelector('.keyboard');
-const textarea = document.querySelector('#textarea');
-const fn = document.querySelector('.Fn');
-const ShiftLeft = document.querySelector('.ShiftLeft');
-const ShiftRight = document.querySelector('.ShiftRight');
-const capsLock = document.querySelector('.CapsLock');
-const ctrl = document.querySelector('.ControlLeft');
-const space = document.querySelector('.Space');
 
-textarea.focus();
-textarea.addEventListener('blur', () => textarea.focus());
-textarea.addEventListener('keypress', (evt) => {
-    evt.preventDefault();
-})
-
-keyboard.addEventListener('mousedown', pressKey);
-window.addEventListener('keydown', pressKey);
-
-fn.addEventListener('click', changeLayout);
-
-ShiftLeft.addEventListener('mousedown', caseSwich);
-ShiftRight.addEventListener('mousedown', caseSwich);
-
-window.addEventListener('keydown', (evt) => {
-    if (evt.code === 'ShiftLeft' || evt.code === 'ShiftRight') {
-        caseSwich(evt);
-    }
-
-    if ((evt.code === 'Enter')) {
-        textarea.value += '\n';
-    }
-
-    if ((evt.code === 'Space')) {
-        textarea.value += ' ';
-    }
-
-    if ((evt.code === 'Tab')) {
-        evt.preventDefault();
-        textarea.value += '    ';
-    }
-})
-
-window.addEventListener('keyup', (evt) => {
-    if (evt.code === 'ShiftLeft' || evt.code === 'ShiftRight') {
-        caseSwich(evt);
-    }
-})
-
-capsLock.addEventListener('click', capsLockSwich);
-
-window.addEventListener('keydown', (evt) => {
-    if (evt.code === 'CapsLock') {
-        capsLockSwich();
-    }
-})
-
-window.addEventListener('keyup', (evt) => {
-    if (evt.code === 'CapsLock') {
-        capsLockSwich();
-    }
-})
-
-function changeLang(...codes) {
-    let pressed = new Set();
-
-    window.addEventListener('keydown', (evt) => {
-        pressed.add(evt.code);
-
-        for (let code of codes) {
-            if (!pressed.has(code)) {
-                return;
-            }
-        }
-
-        pressed.clear();
-
-        changeLayout();
-    });
-
-    window.addEventListener('keyup', (evt) => {
-        pressed.delete(evt.code);
-    });
-}
-
-changeLang('Space', 'ControlLeft')
+addWindowListeners();
+addKeyListeners();
---
+++ src/JS/listeners.js
@@ -0,0 +1,82 @@
+import { pressKey } from './press.js';
+import { changeLayout, caseSwich, capsLockSwich } from './swich.js';
+
+
+function addWindowListeners() {
+    let pressed = new Set();
+
+    window.addEventListener('mousedown', pressKey);
+    window.addEventListener('keydown', pressKey);
+
+    window.addEventListener('keydown', (evt) => {
+        if (evt.code === 'ShiftLeft' || evt.code === 'ShiftRight') {
+            caseSwich(evt);
+        }
+
+        if (evt.code === 'CapsLock') {
+            capsLockSwich();
+        }
+
+        if ((evt.code === 'Enter')) {
+            textarea.value += '\n';
+        }
+
+        if ((evt.code === 'Space')) {
+            textarea.value += ' ';
+        }
+
+        if ((evt.code === 'Tab')) {
+            evt.preventDefault();
+            textarea.value += '    ';
+        }
+    })
+
+    window.addEventListener('keyup', (evt) => {
+        if (evt.code === 'ShiftLeft' || evt.code === 'ShiftRight') {
+            caseSwich(evt);
+        }
+
+        if (evt.code === 'CapsLock') {
+            capsLockSwich();
+        }
+
+        pressed.delete(evt.code);
+    })
+
+
+    window.addEventListener('keydown', (evt) => {
+        pressed.add(evt.code);
+
+        if (!pressed.has('Space') || !pressed.has('ControlLeft')) {
+            return;
+        }
+
+        pressed.clear();
+
+        changeLayout();
+    });
+}
+
+function addKeyListeners() {
+    const textarea = document.querySelector('#textarea');
+    const fn = document.querySelector('.Fn');
+    const ShiftLeft = document.querySelector('.ShiftLeft');
+    const ShiftRight = document.querySelector('.ShiftRight');
+    const capsLock = document.querySelector('.CapsLock');
+
+    textarea.focus();
+    textarea.addEventListener('blur', () => textarea.focus());
+    textarea.addEventListener('keypress', (evt) => {
+        evt.preventDefault();
+    })
+
+    fn.addEventListener('click', changeLayout);
+
+    ShiftLeft.addEventListener('mousedown', caseSwich);
+    ShiftRight.addEventListener('mousedown', caseSwich);
+
+    capsLock.addEventListener('click', capsLockSwich);
+
+}
+
+export { addWindowListeners, addKeyListeners }
